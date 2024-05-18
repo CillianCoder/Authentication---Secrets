61b6f2fafadd8814982656e98f444492fe0aef8d
Madushan Sandakelum <75947205+CillianCoder@users.noreply.github.com>
2024-05-18 13:17:08 +0530

    level 04 - Salting and Hashing Passwords with bcrypt

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require('md5');
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
 
 const app = express();
 
@@ -37,31 +38,42 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-    newUser.save()
-        .then(() => {
-            res.render("secrets");
-        })
-        .catch((err) => {
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+        if (err) {
             console.log(err);
-        })
+        } else {
+            const newUser = new User({
+                email: req.body.username,
+                password: hash
+            });
+            newUser.save()
+                .then(() => {
+                    res.render("secrets");
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+        }
+    });
 });
 
 app.post("/login", (req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
-    User.findOne({email: username})
-    .then((foundUser) => {
-        if(foundUser.password === password){
-            res.render("secrets");
-        }else{
-            console.log("Password didn't match");
-        }
-    })
+    User.findOne({ email: username })
+        .then((foundUser) => {
+            bcrypt.compare(password, foundUser.password, function (err, result) {
+                if (err) {
+                    console.log(err);
+                } else if (result) {
+                    res.render("secrets");
+                } else {
+                    // Passwords don't match
+                    console.log('Passwords do not match');
+                }
+            });
+        })
         .catch((err) => {
             console.log(err);
         })

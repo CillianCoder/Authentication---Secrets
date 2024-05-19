d211c5dd315c64e9d37413b71b1b93940b207e7f
Madushan Sandakelum <75947205+CillianCoder@users.noreply.github.com>
2024-05-19 17:09:35 +0530

    level 6 - OAuth 2.0 & How to Implement Sign In with Google and Facebook

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const https = require('https');
+const fs = require('fs');
 require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -6,6 +8,9 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
 
@@ -28,17 +33,77 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 
 const userSchema = new mongoose.Schema({
     username: String,
-    password: String
+    password: String,
+    googleId: String,
+    facebookId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, {
+            id: user.id,
+            username: user.username,
+            picture: user.picture
+        });
+    });
+});
+
+passport.deserializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "https://localhost:3000/auth/google/secrets"
+},
+    function (accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
+
+passport.use(new FacebookStrategy({
+    clientID: process.env.APP_ID,
+    clientSecret: process.env.APP_SECRET,
+    callbackURL: 'https://localhost:3000/auth/facebook/secrets'
+},
+    function (accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }));
+
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ["profile"] })
+);
+
+app.get("/auth/facebook",
+    passport.authenticate('facebook')
+);
+
+app.get('/auth/google/secrets',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function (req, res) {
+        res.redirect("/secrets");
+    });
+
+app.get('/auth/facebook/secrets',
+    passport.authenticate('facebook', { failureRedirect: '/login' }),
+    function (req, res) {
+        res.redirect('/secrets');
+    }
+);
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -102,6 +167,11 @@ app.post("/login", (req, res) => {
 
 
 
-app.listen(3000, () => {
-    console.log("Server started port 3000")
+const options = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+};
+
+https.createServer(options, app).listen(3000, () => {
+    console.log('Server started on https://localhost:3000');
 });
